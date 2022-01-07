3b373826084414b3c4f520449c0db2c302115da2
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-07 10:16:02 +0300

    This commit will add name to form.

--- QuoteScope/src/app/app.module.ts
+++ QuoteScope/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
