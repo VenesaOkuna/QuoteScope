fa38c4be2b555731ccf63d9f451dfe646b3bf667
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-07 11:05:39 +0300

    This commit will add directive.

---
+++ QuoteScope/src/app/highlight-quote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightQouteDirective } from '/home/venesa/Documents/school_projects/CORE/QuoteScope/QuoteScope/src/app/highlight-quote.directive';
+
+describe('HighlightQouteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightQouteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ QuoteScope/src/app/highlight-quote.directive.ts
@@ -0,0 +1,22 @@
+
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightQoute]'
+})
+export class HighlightQouteDirective {
+
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
+
+}
