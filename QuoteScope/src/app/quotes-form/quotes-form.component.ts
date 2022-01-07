f0c2d7389a8992a04943b2b23fbbe39c489517b2
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-07 10:47:51 +0300

    This commit will add quote emmiter function to form.

--- QuoteScope/src/app/quotes-form/quotes-form.component.ts
+++ QuoteScope/src/app/quotes-form/quotes-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
   styleUrls: ['./quotes-form.component.css']
 })
-export class QuotesFormComponent implements OnInit {
 
+export class QuotesFormComponent implements OnInit {
+  quoted = new Quote(0,"","","","",new Date(),0,0);
+  @Output() add = new EventEmitter<Quote>();
+
+  addQuote(){
+this.add.emit(this.quoted);
+this.quoted = new Quote(0,"","","","",new Date(),0,0);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
