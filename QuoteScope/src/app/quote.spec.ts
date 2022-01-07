344076a007872c5373c88b0e3199cf5ba5f6be6a
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-07 09:30:29 +0300

    This commit will add global Quote class.

---
+++ QuoteScope/src/app/quote.spec.ts
@@ -0,0 +1,7 @@
+import { Quote } from '/home/venesa/Documents/school_projects/CORE/QuoteScope/QuoteScope/src/app/quote';
+
+describe('Quote', () => {
+  it('should create an instance', () => {
+    expect(new Quote()).toBeTruthy();
+  });
+});
--- QuoteScope/src/app/quotes/quotes.component.ts
+++ QuoteScope/src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '/home/venesa/Documents/school_projects/CORE/QuoteScope/QuoteScope/src/app/quote';
 
 @Component({
   selector: 'app-quotes',
@@ -14,10 +15,18 @@ export class QuotesComponent implements OnInit {
     new Quote (3,'Grace','Softwares','Software and cathedrals are much the same — first we build them, then we pray.','reddit user',new Date(2022,6,3),0,0),
     new Quote (4,'Ken','Life','I always wanted to be somebody, but now I realize I should have been more specific.','Lily Tomlin',new Date(2022,7,7),0,0),
     new Quote (5,'Linda','Luck','Luck is what you have left over after you give 100 percent.','Langston Coleman',new Date(2022,7,17),0,0),
-    new Quote (6,'Ness','Wit','A person with a sharp tongue will eventually cut themselves.','J.Robson Koenig',new Date(2022,7,15),0,0)
+    new Quote (6,'Ness','Wit','A person with a sharp tongue will eventually cut themselves.','J.Robson Koenig',new Date(2022,7,15),0,0),
     new Quote (7,'Brian','Pretending','Pretending to care what men think is an art. It takes moments to learn, but lifetimes to master. Id like to believe Im an expert.','Dennis Sharpe',new Date(2022,7,17),0,0),
     new Quote (8,'Hellen','Awareness','The important thing to remember is not to forget.','Benny Bellamacina',new Date(2022,7,15),0,0)
   ]; 
+
+  //sorting object array by date property
+
+  get sortQuotes() {
+    return this.quotes.sort((a, b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }  
 
   addedQuote(quote: { id: number; completeDate: string | number | Date; }){
     let arraysize = this.quotes.length;
@@ -40,7 +49,7 @@ export class QuotesComponent implements OnInit {
  
   // display
 
-  displayInfo(index: string | number){
+  displayInfo(index: number){
     this.quotes[index].showInfo = !this.quotes[index].showInfo;
   }
 
